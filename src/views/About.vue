27552b1f6efb25801253a5edf5099546166680a6
ByeongJoo <rewg3@naver.com>
2019-09-10 13:13:48 +0900

    click test

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,34 @@
 <template>
   <div class="about">
     <h1>This is an about pages</h1>
-    <router-view></router-view>
+    <button v-on:click="yesView">click</button><br><br>
+    <Test-Comp v-if="isView"></Test-Comp>
+    <Test-Comp2 v-else></Test-Comp2>
   </div>
 </template>
+
+<script>
+import TestComp from './test1'
+import TestComp2 from './test2'
+export default {
+  components: {
+    TestComp,
+    TestComp2
+  },
+  data: () => {
+    return {
+      isView: false
+    }
+  },
+  methods: {
+    yesView: function() {
+      const _this = this
+      _this.isView = !_this.isView
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
