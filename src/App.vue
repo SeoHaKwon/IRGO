ee4b15c41c19879a9c88ae880d4d5a84839e869f
ByeongJoo <rewg3@naver.com>
2019-09-18 11:31:43 +0900

    change swipe

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
+      <router-link to="/swipe">Swipe</router-link> |
       <router-link to="/capture">Capture</router-link>
     </div>
     <router-view/>
--- src/routes/router.js
+++ src/routes/router.js
@@ -16,12 +16,12 @@ export default new Router({
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/swipe',
+      name: 'swipe',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ '../views/About'),
+      component: () => import(/* webpackChunkName: "about" */ '../views/Swipe'),
       children: [
         {
           path: 'test',
