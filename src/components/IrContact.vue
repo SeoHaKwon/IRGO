cd5cfe18ca968ab7adc074bc96d95e1b798c9347
0chan <kimynam1@naver.com>
2019-12-13 17:41:50 +0900

    모바일작업

--- src/components/IrContact.vue
+++ src/components/IrContact.vue
@@ -5,10 +5,13 @@
       <div class="company-info">
         <p class="call en">02-3773-1114</p>
         <p class="email en">jsb@lgdisplay.co kr</p>
-        <p class="address">서울시 영등포구 국제금융로 8길 2 농협재단빌딩 14F(여의도동)</p>
+        <p class="address">
+          서울시 영등포구 국제금융로 8길 2 농협재단빌딩 14F(여의도동)
+        </p>
       </div>
     </div>
     <div class="contact-section">
+      <p class="title-wrap"><strong>R 전용 App</strong></p>
       <div class="app-download">
         <div class="app-box">
           <div class="txt-box google">
@@ -45,134 +48,216 @@
 </template>
 
 <script>
-import SubVisual from '@/components/SubVisual.vue'
-import Footer from '@/components/Footer.vue'
+import SubVisual from "@/components/SubVisual.vue";
+import Footer from "@/components/Footer.vue";
 
 export default {
-  name: 'status',
+  name: "status",
   components: {
     SubVisual,
     Footer
-  },
-}
-
+  }
+};
 </script>
 
 <style lang="scss">
 @import "@/style/_color.scss";
 @import "@/style/_mixin.scss";
-  .gray-bg {
-    position:relative;
-    &::after {
-      position:absolute;
-      left:0;
-      top:0;
-      content:"";
-      display:inline-block;
-      width:100%;
-      height:321px;
-      background-color:#f3f3f7;
+.gray-bg {
+  position: relative;
+  &::after {
+    position: absolute;
+    left: 0;
+    top: 0;
+    content: "";
+    display: inline-block;
+    width: 100%;
+    height: 321px;
+    background-color: #f3f3f7;
+  }
+}
+.contact-section {
+  & + .contact-section {
+    margin-top: 140px;
+  }
+}
+.app-download {
+  display: flex;
+  .app-box {
+    width: calc(50% - 5px);
+    padding: 66px 60px;
+    border: 1px solid #e5e5e5;
+    border-top: 3px solid $black;
+    & + .app-box {
+      margin-left: 10px;
     }
   }
-  .contact-section {
-    & + .contact-section {
-      margin-top:140px;
-    }
-  }
-  .app-download {
-    display:flex;
-    .app-box {
-      width:calc(50% - 5px);
-      padding:66px 60px;
-      border:1px solid #e5e5e5;
-      border-top:3px solid $black;
-      & + .app-box {
-        margin-left:10px;
+  .txt-box {
+    position: relative;
+    padding-left: 70px;
+    @each $obj in google, apple {
+      &.#{$obj} {
+        @include bgImg(icon/icon_#{$obj}, $pos: left center);
       }
     }
-    .txt-box {
-      position: relative;
-      padding-left:70px;
-      @each $obj in google,apple {
-        &.#{$obj}{
-          @include bgImg(icon/icon_#{$obj},$pos :left center);
+    .app-tit {
+      font-size: 20px;
+      font-weight: 500;
+      color: $black3;
+    }
+    p {
+      margin-top: 10px;
+      font-size: 16px;
+      font-weight: 500;
+      color: $gray8;
+    }
+    .link {
+      position: absolute;
+      right: 0;
+      top: 50%;
+      transform: translateY(-50%);
+      display: inline-block;
+      width: 22px;
+      height: 25px;
+      margin-left: auto;
+      @include bgImg(bg/bg_more_bottom);
+    }
+  }
+}
+.contant-wrap {
+  position: relative;
+  z-index: 5;
+  @include contentWidth($width: 1200px);
+  .company-info {
+    display: flex;
+    flex-wrap: wrap;
+    padding: 90px 100px;
+    border-top: 3px solid $black;
+    background-color: $white;
+    @include shadow();
+    p {
+      width: 50%;
+      padding-left: 44px;
+      font-size: 20px;
+      line-height: 23px;
+      color: $black3;
+      font-weight: 500;
+      @each $obj in call, email, address {
+        &.#{$obj} {
+          @include bgImg(icon/icon_#{$obj}, $pos: left bottom);
         }
       }
-      .app-tit {
-        font-size:20px;
-        font-weight: 500;
-        color:$black3;
-      }
-      p {
-        margin-top:10px;
-        font-size:16px;
-        font-weight: 500;
-        color:$gray8;
-      }
-      .link {
-        position:absolute;
-        right:0;
-        top:50%;
-        transform: translateY(-50%);
-        display:inline-block;
-        width:22px;
-        height:25px;
-        margin-left:auto;
-        @include bgImg(bg/bg_more_bottom);
+      &.address {
+        width: 100%;
+        padding-top: 34px;
+        margin-top: 34px;
+        border-top: 1px solid #f3f3f7;
       }
     }
   }
+  .sns-list {
+    display: flex;
+    padding: 66px 0;
+    justify-content: center;
+    border: 1px solid #e5e5e5;
+    border-top: 3px solid $black;
+    a {
+      display: inline-block;
+      width: 60px;
+      height: 60px;
+      @each $obj in facebook, instagram, twitter, youtube, blog, naver {
+        &.#{$obj} {
+          @include bgImg(icon/icon_#{$obj});
+        }
+      }
+      & + a {
+        margin-left: 104px;
+      }
+    }
+  }
+}
+@media screen and (max-width: 1000px) {
   .contant-wrap {
-    position:relative;
-    z-index: 5;
-    @include contentWidth($width:1200px);
     .company-info {
-      display:flex;
-      flex-wrap:wrap;
-      padding:90px 100px;
-      border-top:3px solid $black;
-      background-color:$white;
-      @include shadow();
+      padding: 50px 30px;
       p {
-        width:50%;
-        padding-left:44px;
-        font-size:20px;
-        line-height:23px;
-        color:$black3;
-        font-weight: 500;
-        @each $obj in call, email,address {
-          &.#{$obj}{
-            @include bgImg(icon/icon_#{$obj},$pos:left bottom);
-          }
-        }
+        width: 100%;
+        padding-left: 25px;
+        font-size: 13px;
+        line-height: 20px;
+        background-size: auto 12px;
+        background-position: left center;
         &.address {
-          width:100%;
-          padding-top:34px;
-          margin-top:34px;
-          border-top:1px solid #f3f3f7;
+          padding-top: 0px;
+          margin-top: 40px;
+          background-position: left center;
+          border-top: none;
+        }
+        & + p {
+          position: relative;
+          margin-top: 40px;
+          &::after {
+            display: block;
+            content: "";
+            position: absolute;
+            left: 0;
+            top: -20px;
+            width: 100%;
+            height: 1px;
+            background-color: #f3f3f7;
+          }
         }
       }
     }
     .sns-list {
-      display:flex;
-      padding:66px 0;
-      justify-content: center;
-      border:1px solid #e5e5e5;
-      border-top:3px solid $black;
+      padding: 10px 0 45px 0;
+      justify-content: cener;
+      flex-wrap: wrap;
       a {
-        display:inline-block;
-        width:60px;
-        height:60px;
-        @each $obj in  facebook, instagram, twitter,youtube,blog,naver {
-          &.#{$obj} {
-            @include bgImg(icon/icon_#{$obj});
-          }
-        }
+        width: 33.33%;
+        margin-top: 35px;
+        background-size: cover;
+        background-size: 32px 32px;
         & + a {
-          margin-left:104px;
+          margin-left: 0;
         }
       }
     }
   }
-  
+  .contact-section {
+    & + .contact-section {
+      margin-top: 70px;
+    }
+  }
+  .app-download {
+    display: block;
+    .app-box {
+      width: 100%;
+      padding: 30px;
+      border-top-width: 1px;
+      .txt-box {
+        padding-left: 40px;
+        background-size: auto 25px;
+        strong {
+          display: inline-block;
+          font-size: 16px;
+        }
+        p {
+          margin-top: 5px;
+          font-size: 10px;
+        }
+        .link {
+          width: 16px;
+          height: 15px;
+          transform: rotate(-90deg);
+          background-size: cover;
+        }
+      }
+      & + .app-box {
+        margin-left: 0;
+        border-top: 1px solid #e5e5e5;
+      }
+    }
+  }
+}
 </style>
--- src/components/RportTab.vue
+++ src/components/RportTab.vue
@@ -1,140 +1,187 @@
 <template>
   <div class="report-wrap">
-      <ul class="report-direct">
-        <li>
-          <p class="report01">정관</p>
+    <ul class="report-direct">
+      <li>
+        <p class="report01">정관</p>
+        <a href="#" class="link"></a>
+      </li>
+      <li>
+        <p class="report02">기업지배구조보고서</p>
+        <a href="#" class="link"></a>
+      </li>
+      <li>
+        <p class="report03">공시정보관리규정</p>
+        <a href="#" class="link"></a>
+      </li>
+    </ul>
+    <div class="tab-section">
+      <div class="select">
+        <span class="selected txt">연도별 경영보고서</span>
+      </div>
+      <div class="tabs">
+        <button type="button" class="tab active">2019</button>
+        <!--클래스 active 주시면 활성화됩니다.-->
+        <button type="button" class="tab">2018</button>
+        <button type="button" class="tab">2017</button>
+      </div>
+    </div>
+    <div class="tab-cont active">
+      <!--클래스 active 주시면 활성화됩니다.-->
+      <div class="material-list report">
+        <div class="material">
+          <strong class="material-title">사업보고서</strong>
+          <a href="#" class="icon-file  pdf">
+            SAMSUNG 19년 사업보고서
+          </a>
           <a href="#" class="link"></a>
-        </li>
-        <li>
-          <p class="report02">기업지배구조보고서</p>
+        </div>
+        <div class="material">
+          <strong class="material-title">기업지배구조보고서</strong>
+          <a href="#" class="icon-file pdf">
+            SAMSUNG 19년 기업지배구조보고서
+          </a>
           <a href="#" class="link"></a>
-        </li>
-        <li>
-          <p class="report03">공시정보관리규정</p>
+        </div>
+        <div class="material">
+          <strong class="material-title">지속가능경영보고서</strong>
+          <a href="#" class="icon-file  pdf">
+            SAMSUNG 19년 지속가능경영보고서
+          </a>
           <a href="#" class="link"></a>
-        </li>
-      </ul>
-      <div class="tab-section">
-        <div class="select">
-          <span class="selected txt">연도별 경영보고서</span>
         </div>
-        <div class="tabs">
-          <button type="button" class="tab active">2019</button> <!--클래스 active 주시면 활성화됩니다.-->
-          <button type="button" class="tab">2018</button>
-          <button type="button" class="tab">2017</button>
+        <div class="material">
+          <strong class="material-title">연차보고서</strong>
+          <a href="#" class="icon-file  pdf">
+            SAMSUNG 19년 연차보고서
+          </a>
+          <a href="#" class="link"></a>
         </div>
-      </div>
-      <div class="tab-cont active"> <!--클래스 active 주시면 활성화됩니다.-->
-        <div class="material-list report">
-          <div class="material">
-            <strong class="material-title">사업보고서</strong>
-            <a href="#" class="icon-file  pdf">
-              SAMSUNG 19년 사업보고서
-            </a>
-            <a href="#" class="link"></a>
-          </div>
-          <div class="material">
-            <strong class="material-title">기업지배구조보고서</strong>
-            <a href="#" class="icon-file pdf">
-              SAMSUNG 19년 기업지배구조보고서
-            </a>
-              <a href="#" class="link"></a>
-          </div>
-          <div class="material">
-            <strong class="material-title">지속가능경영보고서</strong>
-            <a href="#" class="icon-file  pdf">
-              SAMSUNG 19년 지속가능경영보고서
-            </a>
-            <a href="#" class="link"></a>
-          </div>
-          <div class="material">
-            <strong class="material-title">연차보고서</strong>
-            <a href="#" class="icon-file  pdf">
-              SAMSUNG 19년 연차보고서
-            </a>
-            <a href="#" class="link"></a>
-          </div>
-          <div class="material">
-            <strong class="material-title">기업지배구조보고서</strong>
-            <a href="#" class="icon-file  pdf">
-              SAMSUNG 19년 기업지배구조보고서
-            </a>
-            <a href="#" class="link"></a>
-          </div>
-          <div class="material">
-            <strong class="material-title">사업보고서</strong>
-            <a href="#" class="icon-file  pdf">
-              SAMSUNG 19년 사업보고서
-            </a>
-            <a href="#" class="link"></a>
-          </div>
+        <div class="material">
+          <strong class="material-title">기업지배구조보고서</strong>
+          <a href="#" class="icon-file  pdf">
+            SAMSUNG 19년 기업지배구조보고서
+          </a>
+          <a href="#" class="link"></a>
+        </div>
+        <div class="material">
+          <strong class="material-title">사업보고서</strong>
+          <a href="#" class="icon-file  pdf">
+            SAMSUNG 19년 사업보고서
+          </a>
+          <a href="#" class="link"></a>
         </div>
       </div>
-      <div class="tab-cont">
-        
-      </div>
-      <div class="tab-cont">
-        
-      </div>
+    </div>
+    <div class="tab-cont"></div>
+    <div class="tab-cont"></div>
   </div>
 </template>
 
 <script>
-import SubVisual from '@/components/SubVisual.vue'
-import Footer from '@/components/Footer.vue'
+import SubVisual from "@/components/SubVisual.vue";
+import Footer from "@/components/Footer.vue";
 
 export default {
-  name: 'status',
+  name: "status",
   components: {
     SubVisual,
     Footer
-  },
-}
-
+  }
+};
 </script>
 
 <style lang="scss">
 @import "@/style/_color.scss";
 @import "@/style/_mixin.scss";
-  .report-direct {
-    display:flex;
-    border-top:2px solid $black;
-    background-color:$white;
-    @include contentWidth($width:1200px);
-    @include shadow();
-    li {
+.report-direct {
+  display: flex;
+  border-top: 2px solid $black;
+  background-color: $white;
+  @include contentWidth($width: 1200px);
+  @include shadow();
+  li {
+    position: relative;
+    width: 33.33%;
+    padding: 74px 76px 74px 60px;
+    p {
       position: relative;
-      width:33.33%;
-      padding:74px 76px 74px 60px;
-      p {
-        position:relative;
-        padding-left:44px;
-        font-size:20px;
-        line-height:29px;
-        font-weight: 500;
-        color:$black3;
-        @each $obj in report01,report02,report03 {
-          &.#{$obj}{
-            @include bgImg(icon/icon_#{$obj},$pos:left center);
-          }
+      padding-left: 44px;
+      font-size: 20px;
+      line-height: 29px;
+      font-weight: 500;
+      color: $black3;
+      @each $obj in report01, report02, report03 {
+        &.#{$obj} {
+          @include bgImg(icon/icon_#{$obj}, $pos: left center);
         }
       }
-      .link {
-        display:inline-block;
-        position: absolute;
-        right:50px;
-        top:50%;
-        width:26px;
-        height:21px;
-        transform: translateY(-50%);
-        @include bgImg(bg/bg_more_right);
-      }
-      & + li {
-        border-left:1px solid #e9eaec;
-      }
     }
-    & +.tab-section {
-      margin-top:110px;
+    .link {
+      display: inline-block;
+      position: absolute;
+      right: 50px;
+      top: 50%;
+      width: 26px;
+      height: 21px;
+      transform: translateY(-50%);
+      @include bgImg(bg/bg_more_right);
+    }
+    & + li {
+      border-left: 1px solid #e9eaec;
     }
   }
+  & + .tab-section {
+    margin-top: 110px;
+  }
+}
+@media screen and (max-width: 1000px) {
+  .report-wrap {
+    .tab-section {
+      .select {
+        display: none;
+      }
+    }
+  }
+  .report-direct {
+    display: block;
+    li {
+      width: 100%;
+      padding: 30px;
+      & + li {
+        border-top: 1px solid #e9eaec;
+      }
+      p {
+        padding-left: 30px;
+        font-size: 12px;
+        background-size: auto 20px;
+      }
+      .link {
+        width: 16px;
+        height: 15px;
+        right: 30px;
+        background-size: cover;
+      }
+    }
+  }
+  & + .tab-section {
+    margin-top: 54px;
+  }
+  .material-list {
+    .material {
+      padding: 20px 30px;
+      .icon-file {
+        display: none;
+      }
+      .material-title {
+        font-size: 14px;
+        line-height: 30px;
+      }
+      .link {
+        width: 15px;
+        height: 14px;
+        background-size: cover;
+      }
+    }
+  }
+}
 </style>
